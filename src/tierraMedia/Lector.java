0dcd11bd41caeec9b9201cb1e8accff5de1fd7a9
Virginia <Virginia@192.168.0.20>
2021-09-02 19:45:44 -0300

    lector

--- src/tierraMedia/Lector.java
+++ src/tierraMedia/Lector.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package tierraMedia;
 
 import java.io.BufferedReader;
@@ -46,52 +45,3 @@ public class Lector {
 		return usuario;
 	}
 }
-=======
-package tierraMedia;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-
-public class Lector {
-	private FileReader fr = null;
-	private BufferedReader br = null;
-
-	public void leerUsuarios(String archivo) {
-		try {
-			fr = new FileReader(archivo);
-			br = new BufferedReader(fr);
-
-			String linea = br.readLine();
-			while (linea != null) {
-				System.out.println(crearUsuario(linea));
-				linea = br.readLine();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-
-	}
-
-	private Usuario crearUsuario(String linea) {
-		String[] lin = linea.split(",");
-		Usuario usuario;
-		usuario = new Usuario(lin[0], Integer.parseInt(lin[1]), Double.parseDouble(lin[2]),
-				TipoAtraccion.valueOf(lin[3].toUpperCase()));
-		//REVISAR SI ES CORRECTO EL ORDEN EN EL QUE ESTAN LOS DATOS (Nombre, presupuesto, tiempo, tipoPreferido)
-		return usuario;
-	}
-}
->>>>>>> refs/heads/master
