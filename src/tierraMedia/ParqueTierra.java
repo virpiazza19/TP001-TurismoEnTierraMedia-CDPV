96522eed205c0f3271b1819b6cfd06837d41a419
AgusDemelli <agustinademelli@gmail.com>
2021-09-04 13:02:04 -0300

    Se agregan las excepciones para la lectura de usuarios Se agrega el lectorUsuario al main de ParqueTIerra

--- src/tierraMedia/ParqueTierra.java
+++ src/tierraMedia/ParqueTierra.java
@@ -1,15 +1,16 @@
 package tierraMedia;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
-public class ParqueTierra { //Clase general que guarda las dos listas
-	
-	private List <Usuario> usuarios = new LinkedList<Usuario>();
-	private List <Producto> ofertas = new ArrayList<Producto>(); //La puse como ArrayList para poder ordenarla 
-	
-	public void main(String[]args) {
-		//Falta la invocación de los lectores y más
+public class ParqueTierra { // Clase general que guarda las dos listas
+
+	private List<Usuario> usuarios;
+	private List<Producto> ofertas = new ArrayList<Producto>(); // La puse como ArrayList para poder ordenarla
+
+	public static void main(String[] args) {
+		ParqueTierra parqueTierra = new ParqueTierra();
+		LectorUsuario lector = new LectorUsuario();
+		parqueTierra.usuarios = lector.leerUsuarios("archivos/usuarios.csv");
 	}
 }
