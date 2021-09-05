dbfb9843c6641e6872e5bcab9b6cef80cbcedd6d
AgusDemelli <agustinademelli@gmail.com>
2021-09-05 13:28:37 -0300

    Revert "Revert "Revert "Se agregan las excepciones para lectura de usuarios"""
    This reverts commit 10afe603ae1ee6ddead9a490fe0b3dbb56d81411.

--- src/tierraMedia/ParqueTierra.java
+++ src/tierraMedia/ParqueTierra.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ParqueTierra { // Clase general que guarda las dos listas
-
-	private List<Usuario> usuarios = new LinkedList<Usuario>();
-	private List<Producto> ofertas = new ArrayList<Producto>(); // La puse como ArrayList para poder ordenarla
-
-	public static void main(String[] args) {
-		ParqueTierra parqueTierra = new ParqueTierra();
-		LectorDeUsuarios lector = new LectorDeUsuarios();
-		parqueTierra.usuarios = lector.leerUsuarios("archivos/usuarios.csv");
-		// Falta la invocación de los lectores y más
+public class ParqueTierra { //Clase general que guarda las dos listas
+	
+	private List <Usuario> usuarios = new LinkedList<Usuario>();
+	private List <Producto> ofertas = new ArrayList<Producto>(); //La puse como ArrayList para poder ordenarla 
+	
+	public void main(String[]args) {
+		//Falta la invocación de los lectores y más
 	}
 }
