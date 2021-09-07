b2941790e5481b3d132a4c80b4399d69ddfc36df
Virginia <virginiapiazza19@gmail.com>
2021-09-07 20:57:31 -0300

    Se extrae método del comparador para simplicarlo

--- src/tierraMedia/ComparadorPorTipoAtraccion.java
+++ src/tierraMedia/ComparadorPorTipoAtraccion.java
@@ -13,27 +13,15 @@ public class ComparadorPorTipoAtraccion implements Comparator<Producto> {
 	public int compare(Producto P1, Producto P2) {
 		if (P1.preferida == this.tipoPreferido && P2.preferida == this.tipoPreferido) {
 			if (P1.esPromo() && P2.esPromo()) {
-				if (Integer.compare(P1.costo, P2.costo) == 0) {
-					return -Double.compare(P1.duracion, P2.duracion);
-				} else {
-					return -Double.compare(P1.costo, P2.costo);
-				}
+				return comparaPorCostoyPorTiempo(P1, P2);
 			} else {
 				return -Boolean.compare(P1.esPromo(), P2.esPromo());
 			}
 		} else if (P1.preferida != this.tipoPreferido && P2.preferida != this.tipoPreferido) {
 			if (P1.esPromo() && P2.esPromo()) {
-				if (Integer.compare(P1.costo, P2.costo) == 0) {
-					return -Double.compare(P1.duracion, P2.duracion);
-				} else {
-					return -Double.compare(P1.costo, P2.costo);
-				}
+				return comparaPorCostoyPorTiempo(P1, P2);
 			} else if (!P1.esPromo() && !P2.esPromo()) {
-				if (Integer.compare(P1.costo, P2.costo) == 0) {
-					return -Double.compare(P1.duracion, P2.duracion);
-				} else {
-					return -Double.compare(P1.costo, P2.costo);
-				}
+				return comparaPorCostoyPorTiempo(P1, P2);
 			} else {
 				return -Boolean.compare(P1.esPromo(), P2.esPromo());
 			}
@@ -41,6 +29,14 @@ public class ComparadorPorTipoAtraccion implements Comparator<Producto> {
 			if (P1.preferida == this.tipoPreferido)
 				return -1;
 			return 1;
+		}
+	}
+
+	private int comparaPorCostoyPorTiempo(Producto P1, Producto P2) {
+		if (Integer.compare(P1.costo, P2.costo) == 0) {
+			return -Double.compare(P1.duracion, P2.duracion);
+		} else {
+			return -Double.compare(P1.costo, P2.costo);
 		}
 	}
 
