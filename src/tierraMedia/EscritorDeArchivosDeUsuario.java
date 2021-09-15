7d66178a564bba0150ccec79d7d22df70a5892ca
Usuario <Usuario@192.168.0.135>
2021-09-15 01:08:43 -0300

    se modifica el escritor de archivos

--- src/tierraMedia/EscritorDeArchivosDeUsuario.java
+++ src/tierraMedia/EscritorDeArchivosDeUsuario.java
@@ -9,27 +9,23 @@ import java.util.List;
 
 public class EscritorDeArchivosDeUsuario {
 
-	public void crearArchivoDeSalida() throws IOException {
-		List<Producto> itinerario = new ArrayList<Producto>();
-		List<Usuario> usuarios = new LinkedList<Usuario>();
-		
-		//HABRIA QUE CONSULTARLE AL PROFE ALGUNA FORMA DE NO TENER QUE DECLARAR LAS LISTAS EN TODAS LAS CLASES
-
-		BufferedWriter bw = new BufferedWriter(new FileWriter("archivosDeSalida/nombre.out")); 
-		//NO SE COMO HACER PARA QUE EL ARCHIVO TENGA EL NOMBRE DEL USUARIO
+	public void crearArchivoDeSalida(List<Usuario> usuarios, String archivoSalida) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter(archivoSalida));
+		// la ruta la ingresamos al metodo invocar al metodo
+		// -> EscritorDeArchivosDeUsuario.crearArchivoDeSalida(usuarios,
+		// "archivosDeSalida/" + u.getNombre() + ".csv");
 
 		for (Usuario U : usuarios) {
-			bw.write("Nombre del usuario: " + U.getNombre() + ", Presupuesto total: " + U.getPresupuesto()
-					+ ", Tiempo disponible: " + U.getTiempoDisponible());
-			bw.newLine();
+			bw.write("Nombre del usuario: " + U.getNombre() + " \nPresupuesto total: " + U.getPresupuesto()
+					+ " monedas de oro \nTiempo disponible: " + U.getTiempoDisponible() + " horas\n");
 			bw.write("Itinerario: ");
 			bw.newLine();
 
 			double duracionTotal = 0;
 			int costoTotal = 0;
-			for (Producto P : itinerario) { 
+			for (Producto P : U.getItinerario()) {
 				if (P.esPromo()) {
-					bw.write(P.nombre + ", atracciones incluidas: " + P.atracciones);
+					bw.write(P.nombre + ". Atracciones incluidas: " + P.atracciones);
 					bw.newLine();
 				}
 				if (!P.esPromo()) {
@@ -39,10 +35,9 @@ public class EscritorDeArchivosDeUsuario {
 				duracionTotal += P.duracion;
 				costoTotal += P.costo;
 			}
-			bw.write("El costo total del itinerario es de " + costoTotal + " monedas, y su duración total es de "
-					+ duracionTotal + " horas.");
-
-			bw.close();
+			bw.write("Costo total: " + duracionTotal + " monedas de oro\n");
+			bw.write("Tiempo necesario para realizar el itinerario completo: " + costoTotal + " horas\n");
 		}
+		bw.close();
 	}
 }
